cd3c404cc0d9eebd5cdb4de8150310da6b4626b2
Jacob C. Norkus <jcnorkus@csis.svsu.edu>
2017-01-23 11:43:17 -0500

    First Commit

---
+++ crud/create.php
@@ -0,0 +1,108 @@
+<?php 
+	
+	require 'database.php';
+
+	if ( !empty($_POST)) {
+		// keep track validation errors
+		$dateError = null;
+		$timeError = null;
+		$locationError = null;
+		$descriptionError = null;
+		
+		// keep track post values
+		$date = $_POST['event_date'];
+		$time = $_POST['event_time'];
+		$location = $_POST['event_location'];
+		$description = $_POST['event_description'];
+		
+		// validate input
+		$valid = true;
+		if (empty($date)) {
+			$dateError = 'Please enter Date';
+			$valid = false;
+		}
+		
+		if (empty($time)) {
+			$timeError = 'Please enter Time';
+			$valid = false;
+		} 
+		
+		if (empty($location)) {
+			$locationError = 'Please enter location';
+			$valid = false;
+		}
+		
+		if (empty($description)) {
+			$descriptionError = 'Please enter Description';
+			$valid = false;
+		}
+		
+		
+		// insert data
+		if ($valid) {
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "INSERT INTO events (date,time,location, description) values(?, ?, ?, ?)";
+			$q = $pdo->prepare($sql);
+			$q->execute(array($date,$time,$location,$description));
+			Database::disconnect();
+			header("Location: events.php");
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+    
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Create a Customer</h3>
+		    		</div>
+    		
+	    			<form class="form-horizontal" action="create.php" method="post">
+					  <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+					    <label class="control-label">Name</label>
+					    <div class="controls">
+					      	<input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
+					      	<?php if (!empty($nameError)): ?>
+					      		<span class="help-inline"><?php echo $nameError;?></span>
+					      	<?php endif; ?>
+					    </div>
+					  </div>
+					  <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+					    <label class="control-label">Email Address</label>
+					    <div class="controls">
+					      	<input name="email" type="text" placeholder="Email Address" value="<?php echo !empty($email)?$email:'';?>">
+					      	<?php if (!empty($emailError)): ?>
+					      		<span class="help-inline"><?php echo $emailError;?></span>
+					      	<?php endif;?>
+					    </div>
+					  </div>
+					  <div class="control-group <?php echo !empty($mobileError)?'error':'';?>">
+					    <label class="control-label">Mobile Number</label>
+					    <div class="controls">
+					      	<input name="mobile" type="text"  placeholder="Mobile Number" value="<?php echo !empty($mobile)?$mobile:'';?>">
+					      	<?php if (!empty($mobileError)): ?>
+					      		<span class="help-inline"><?php echo $mobileError;?></span>
+					      	<?php endif;?>
+					    </div>
+					  </div>
+					  <div class="form-actions">
+						  <button type="submit" class="btn btn-success">Create</button>
+						  <a class="btn" href="index.php">Back</a>
+						</div>
+					</form>
+				</div>
+				
+    </div> <!-- /container -->
+  </body>
+</html>
